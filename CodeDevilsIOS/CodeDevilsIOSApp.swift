94500e1ee64bd2abc3ac743b118d9357869bfba6
Korbin101lee <khillan5223@gmail.com>
2025-05-11 18:18:28 -0700

    Initial commit of Xcode iOS project excluding user-specific files

---
+++ CodeDevilsIOS/CodeDevilsIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeDevilsIOSApp.swift
+//  CodeDevilsIOS
+//
+//  Created by Korbin Hillan on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeDevilsIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeDevilsIOSTests/CodeDevilsIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeDevilsIOSTests.swift
+//  CodeDevilsIOSTests
+//
+//  Created by Korbin Hillan on 5/11/25.
+//
+
+import Testing
+@testable import CodeDevilsIOS
+
+struct CodeDevilsIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
